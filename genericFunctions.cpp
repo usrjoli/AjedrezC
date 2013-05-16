edaae622284e7055ac715fc97d347489f2e58e02
jose <joseartola@gmail.com>
2013-05-15 22:28:05 -0300

    creacion de find

---
+++ genericFunctions.cpp
@@ -0,0 +1,27 @@
+
+
+#ifndef _CRT_SECURE_NO_DEPRECATE  // suppress MS security warnings
+#define _CRT_SECURE_NO_DEPRECATE
+#endif
+
+#include <iostream>
+
+int find(char* source, char element){
+	int j;
+	bool fin;
+
+	fin = false;
+	j = 0;
+	while ((j < strlen(source)) && (!fin)){
+		if (source[j] == element){
+			fin = true;
+		}else {
+			j++;
+		}
+	}
+	if (fin) {
+		return j;
+	}else{
+		return -1;
+	}
+}
